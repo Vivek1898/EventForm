34675a03f3b8538860833e882fb39b407b8f0d95
Vivek Singh <viveksingh27795@gmail.com>
2022-04-03 13:39:33 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Form from './Components/Form/form';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
   return (
-    <div><Form/></div>
+    <div>
+      <ToastContainer position='top-center'/>
+      <Form/>
+      </div>
   )
 }
 
--- src/Components/Form/form.jsx
+++ src/Components/Form/form.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
 import './form.css';
-
+import {useState} from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 const Form = () => {
+  const [Name, setName] = useState('');
+  const [Email, setEmail] = useState('');
+  const [Mobile, setMobile] = useState('');
+  const [Whatsapp, setWhatsapp] = useState('');
+  const [Institute, setInstitute] = useState('');
+  const [Branch, setBranch] = useState('');
+  const [Year, setYear] = useState('');
+
+
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    
+  
+const  formData = {Name,Email,Mobile,Whatsapp,Institute,Branch,Year};
+console.log(formData);
+
+ axios.post("https://sheet.best/api/sheets/1b1a7f92-1b0f-49a1-bd5e-a3510daffb9b",formData)
+ .then((response)=>{
+   console.log(response);
+ });
+ toast("Congratulation " +Name+ " Registration Sucessfull")
+ e.target.reset();
+
+}
+
   return (
     <div>
       <section id="contact">
@@ -12,36 +39,36 @@ const Form = () => {
       <h3>REGISTER</h3>
     </div>
     <div className="contact-form-wrapper">
-      <form>
-        <div className="form-item">
-          <input type="text" name="sender" required/>
+      <form onSubmit={handleSubmit}>
+        <div className="form-item" id='create-course-form'>
+          <input type="text" onChange={(e) => setName(e.target.value)}  required/>
           <label>Name:</label>
         </div>
         <div className="form-item">
-          <input type="text" name="email" required/>
+          <input type="text" onChange={(e) => setEmail(e.target.value)}  required/>
           <label>Email:</label>
         </div>
         <div className="form-item">
-          <input type="number" name="sender" required/>
+          <input type="number" onChange={(e) => setMobile(e.target.value)}  required/>
           <label>Mobile Number:</label>
         </div>
         <div className="form-item">
-          <input type="number" name="sender" required/>
+          <input type="number" onChange={(e) => setWhatsapp(e.target.value)}  required/>
           <label>Whatsapp Number:</label>
         </div>
         <div className="form-item">
-          <input type="text" name="sender" required/>
+          <input type="text" onChange={(e) => setInstitute(e.target.value)}  required/>
           <label>Institution:</label>
         </div>
         <div className="form-item">
-          <input type="text" name="sender" required/>
+          <input type="text" onChange={(e) => setBranch(e.target.value)} required/>
           <label>Branch:</label>
         </div>
         <div className="form-item">
-          <input type="number" name="sender" required/>
+          <input type="number" onChange={(e) => setYear(e.target.value)}  required/>
           <label>Year:</label>
         </div>
-        <button className="submit-btn">Register</button>  
+        <button className="submit-btn" type='submit' >Register</button>  
       </form>
     </div>
   </div>
